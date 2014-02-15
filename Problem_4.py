9b42808e9a619e2eaf9fe8249ff7733af9f0d76d
Mohamed Elsherif <Bashmohandes@live.com>
2014-02-15 09:55:29 -0800

    Solution for Problem 4
    Using Python

---
+++ Problem_4.py
@@ -0,0 +1,26 @@
+__author__ = 'Mohamed'
+
+def is_palindrome(num):
+    return num == reverse_num(num)
+
+def reverse_num(num):
+    r = 0
+    while num > 0:
+        r = r * 10 + num % 10
+        num /= 10
+    return r
+
+def main():
+    start_range = 100
+    end_range = 1000
+    max_palindrome = 0
+    for i in range(start_range, end_range):
+        for j in range(i, end_range):
+            product = i * j
+            if is_palindrome(product):
+                max_palindrome = max(max_palindrome, product)
+    print max_palindrome
+
+
+if __name__ == "__main__":
+    main()
