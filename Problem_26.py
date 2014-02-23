fda192e3473e36e60711031a93aff6b42e9a5f28
Mohamed Elsherif <bashmohandes@live.com>
2014-02-22 16:43:21 -0800

    Problem 26

---
+++ Problem_26.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+from AutoMeasure import AutoMeasure
+
+@AutoMeasure
+def main():
+	max_seq = 0
+	max_i = 0
+	for i in range(10, 0, -1):
+		if max_seq >= i:
+			break
+		reminders = [ 0 for x in range(i) ]
+		value = 1
+		pos = 0
+
+		while not reminders[value] and value != 0:			
+			reminders[value] = pos
+			value *= 10
+			value %= i			
+			pos += 1
+		print i,reminders
+		if pos - reminders[value] > max_seq:
+			max_seq = pos -reminders[value]
+			max_i = i
+
+	print max_i, max_seq
+
+if __name__ == "__main__":
+	main()
+
