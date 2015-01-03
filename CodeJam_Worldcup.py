e671d2e73d3b9647f123669e7dacb87b166de56b
Mohamed Elsherif <Bashmohandes@live.com>
2015-01-02 22:34:56 -0800

    Unfinished stuff, pushing to save

--- CodeJam_Worldcup.py
+++ CodeJam_Worldcup.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
 
 from AutoMeasure import AutoMeasure
+import sys
 
 __author__ = 'Mohamed.Elsherif'
 
 
 @AutoMeasure
 def main():
-    problems = parse_file("B-small-practice.in")
+    problems = parse_file("B-large-practice.in")
     write_output_file(solve(problems))
 
 
@@ -27,14 +28,16 @@ def parse_file(file_name):
             m = [int(s) for s in input.readline().rstrip('\n').split(' ')]
             tickets = {}
             for l in range(p):
-                tickets[l] = [ int(price) for price in input.readline().rstrip('\n').split(' ')]
+                tickets[l] = [ Ticket(int(price)) for price in input.readline().rstrip('\n').split(' ')]
             result.append(Problem(p, m, tickets))
     return result
+
 
 def write_output_file(result):
     with open("worldcup.out", "w") as output:
         for i in range(len(result)):
             output.write(format("Case #%d: %d\n" % (i + 1, result[i])))
+
 
 class Problem:
     def __init__(self, p, m, tickets):
@@ -44,28 +47,31 @@ class Problem:
         self._teams = 1 << p
         self._team_matches = self._get_team_matches()
 
-    @property
-    def M(self):
-        return self._M
-
-    @property
-    def Tickets(self):
-        return self._tickets
-
-    @property
-    def P(self):
-        return self._p
-
     def solve(self):
         cost = 0
-        for l in range(self.P):
-            must = self._get_min_and_decrease()
-            must_matches = set()
-            for m in must:
-                must_matches.add(self._team_matches[m][l])
-            for m in must_matches:
-                cost += self._tickets[l][m]
+        for round in range(self._p):
+            must_teams = self._get_min_and_decrease()
+            for team in must_teams:
+                if self._is_covered(team, round):
+                    continue
+                ticket = self._get_cheapest_ticket(team, round)
+                if ticket:
+                    cost += ticket.price
+                    ticket.mark_purchased()
         return cost
+
+    def _is_covered(self, team, round):
+        return self._tickets[round][self._team_matches[team][round]].purchased
+
+    def _get_cheapest_ticket(self, team, round):
+        min_price = sys.maxint
+        cheapest_ticket = None
+        for r in range(round + 1):
+            ticket = self._tickets[r][self._team_matches[team][r]]
+            if not ticket.purchased and ticket.price < min_price:
+                min_price = ticket.price
+                cheapest_ticket = ticket
+        return cheapest_ticket
 
     def _get_team_matches(self):
         result = []
@@ -76,18 +82,31 @@ class Problem:
             result.append(matches)
         return result
 
-    def __repr__(self):
-        return format("P=%d, M=%s, tickets=%s, matches=%s" % (self.P, self.M, self.Tickets, self._team_matches))
-
     def _get_min_and_decrease(self):
         result = []
-        for i in range(len(self.M)):
-            if self.M[i] == 0:
+        for i in range(len(self._M)):
+            if self._M[i] == 0:
                 result.append(i)
-            if self.M[i] > 0:
-                self.M[i] -= 1
+            if self._M[i] > 0:
+                self._M[i] -= 1
         return result
 
+
+class Ticket:
+    def __init__(self, price):
+        self._price = price
+        self._purchased = False
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def purchased(self):
+        return self._purchased
+
+    def mark_purchased(self):
+        self._purchased = True
 
 if __name__ == "__main__":
     main()
